dd3a292c56731dcff1cda9f8aeed726e6e32f144
hometogl <usamajob@gmail.com>
2018-08-29 21:25:50 +0530

    123

--- stringc.php
+++ stringc.php
@@ -2,7 +2,16 @@
 $y =12323;
 
 while($y >=  5) {
-    echo "asdf number bading lkjlkj;klj: $x <br>";
+    echo "asdf 1 ";
+    echo "asdf 2 ";
+    echo "asdf 3 ";
+    echo "asdf 4 ";
+    echo "asdf 5 ";
+    echo "asdf 6 ";
+    echo "asdf 7 ";
+    echo "asdf 8 ";
+    echo "asdf 9 ";
+    echo "asdf 10 ";
     $x--;
 }
 ?> 
