002ddbeca314fa31a458be38a24ed58cdd4daead
hometogl <usamajob@gmail.com>
2018-08-29 21:45:37 +0530

    test

--- stringc.php
+++ stringc.php
@@ -8,11 +8,13 @@ while($y >=  5) {
     echo "asdf 4 ";
     echo "asdf 5 ";
     echo "asdf 6 ";
-    echo "asdf 7 ";
+    echo "asdf 87 ";
     echo "asdf 10 ";
     echo "this will be a better one";
     echo "asdf 9 ";
     echo "asdf 60 ";
+
+    echo "asdfasdf";
     $x--;
 }
 ?> 
