098f436461bc98a27c61399f3c32a01d9a95c2d2
hometogl <usamajob@gmail.com>
2018-08-29 16:38:43 +0530

    asdf

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
-<h1>athhar asdfasdfasdf</h1>
-<p>This is a paragraph. asdfasdf </p>
-</body>
-</html> 
+<?php
+class thisIsaclass{
+    public function testFunction(){
+        echo "hi";
+    }
+}
+
+?>
